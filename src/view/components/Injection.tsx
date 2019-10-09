83801e6619c1df0b16ee1a2d35a11a374f346296
Artem Yastrebkov <ayastrebkov@nvidia.com>
2019-10-10 02:18:57 +0300

    Set calendar locale to en-GB

--- src/view/components/Injection.tsx
+++ src/view/components/Injection.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import locale from "date-fns/locale/en-GB";
 import React, { useCallback } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -21,6 +22,7 @@ const Injection: React.FC<InjectionProps> = ({ day, position, onChange }) => {
   return (
     <StyledInjection
       calendarContainer={Calendar}
+      locale={locale}
       withPortal={true}
       selected={day}
       onChange={change}
